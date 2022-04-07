c1e1b4605e7016e16f29ba9d71af14897d09512b
dev-saikumar <saikumarreddyatluri@gmail.com>
2022-04-07 20:51:34 +0530

    cmt fmr sai

--- myfistcode.cpp
+++ myfistcode.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 int main(){
 	// how are you so please understand my stress towards learning
-	//commit frm dev-saikumar
+	//commit frm dev-saikumar how the changes look
 	return 0;
 }
